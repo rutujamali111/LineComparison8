6c78bc73daa26316ce88d1c58fbf9969a158861b
rutuja_mali <rutujamali111@gmail.com>
2022-07-28 06:32:13 +0530

    Calculate length

---
+++ Line.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Line {
+	int x1,x2,y1,y2;
+	Scanner sc=new Scanner(System.in);
+	void InputLengths()
+	{
+		System.out.println("Enter starting co-ordinates of line");
+		int x1=sc.nextInt();
+		 int y1=sc.nextInt();
+		System.out.println("Enter end co-ordinats of line");
+		 int x2=sc.nextInt();
+		 int y2=sc.nextInt();
+	}
+	double LengthOfLine()
+	{
+		InputLengths();
+		double length=Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
+		return length;
+	}
+
+}
---
+++ LineTest.java
@@ -0,0 +1,14 @@
+
+public class LineTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Line l1=new Line();
+		System.out.println("Length of line l1 is:"+l1.LengthOfLine());
+	
+		Line l2=new Line();
+		System.out.println("Length of line l2 is:"+l2.LengthOfLine());
+		
+	}
+
+}
